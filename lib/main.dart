7ecf5c835153366eece3ad251203aa37727e03c5
Akash Soni <akashsoni.6898@gmail.com>
2020-10-11 04:12:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_genius_scan/flutter_genius_scan.dart';
+
+import 'package:open_file/open_file.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    BuildContext context1 = context;
+    return MaterialApp(
+      navigatorKey: Get.key,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('GS SDK Flutter Demo'),
+        ),
+        body: Center(
+          child: Text("No"),
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: GestureDetector(
+              child: Container(
+                height: 50,
+                width: 50,
+                decoration:
+                    BoxDecoration(shape: BoxShape.circle, color: Colors.blue),
+                child: Icon(
+                  Icons.photo_camera_outlined,
+                  color: Colors.white,
+                ),
+              ),
+              onTap: () {
+                FlutterGeniusScan.scanWithConfiguration({
+                  'source': 'camera',
+                  'multiPage': true,
+                }).then((result) {
+                  String pdfUrl = result['pdfUrl'];
+                  OpenFile.open(pdfUrl.replaceAll("file://", ''))
+                      .then((result) => debugPrint(result.toString()),
+                          onError: (error) {
+                    displayError(error);
+                  });
+                }, onError: (error) => displayError(error));
+              }),
+        ),
+      ),
+    );
+  }
+
+  void displayError(PlatformException error) {
+    Get.defaultDialog(
+        title: "Error",
+        middleText: error.message,
+        confirm: FlatButton(
+            onPressed: () {
+              Get.back();
+            },
+            child: Text(
+              "OK",
+              style: TextStyle(color: Colors.blue),
+            )));
+  }
+}
