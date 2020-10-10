33908334847e63daece0721d8290043443907622
Akash Soni <akashsoni.6898@gmail.com>
2020-10-11 04:34:34 +0530

    added provider

---
+++ lib/backend/firebase.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+
+class Server extends ChangeNotifier {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,22 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-
+import 'package:provider/provider.dart';
+import 'backend/firebase.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_genius_scan/flutter_genius_scan.dart';
 
 import 'package:open_file/open_file.dart';
 import 'package:get/get.dart';
+import 'ui/homepage.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    BuildContext context1 = context;
-    return MaterialApp(
-      navigatorKey: Get.key,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('GS SDK Flutter Demo'),
-        ),
-        body: Center(
-          child: Text("No"),
-        ),
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: GestureDetector(
-              child: Container(
-                height: 50,
-                width: 50,
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: Colors.blue),
-                child: Icon(
-                  Icons.photo_camera_outlined,
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {
-                FlutterGeniusScan.scanWithConfiguration({
-                  'source': 'camera',
-                  'multiPage': true,
-                }).then((result) {
-                  String pdfUrl = result['pdfUrl'];
-                  OpenFile.open(pdfUrl.replaceAll("file://", ''))
-                      .then((result) => debugPrint(result.toString()),
-                          onError: (error) {
-                    displayError(error);
-                  });
-                }, onError: (error) => displayError(error));
-              }),
-        ),
-      ),
+    return Provider<Server>(
+      create: (_) => Server(),
+      child: HomePage(),
     );
-  }
-
-  void displayError(PlatformException error) {
-    Get.defaultDialog(
-        title: "Error",
-        middleText: error.message,
-        confirm: FlatButton(
-            onPressed: () {
-              Get.back();
-            },
-            child: Text(
-              "OK",
-              style: TextStyle(color: Colors.blue),
-            )));
   }
 }
